1d46856eabd3ee49f34d65df3d33dcd9480356e1
David Perez Serrada <dserrada@gmail.com>
2026-04-19 00:00:20 +0200

    Interfaz cli

--- projects/ticket-digital-data/src/main/java/org/terra/incognita/ticketdigital/mercadona/Main.java
+++ projects/ticket-digital-data/src/main/java/org/terra/incognita/ticketdigital/mercadona/Main.java
@@ -1,7 +1,41 @@
 package org.terra.incognita.ticketdigital.mercadona;
 
-public class Main {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+
+@Command(name = "ticket-digital", 
+         mixinStandardHelpOptions = true, 
+         version = "1.0",
+         description = "Interfaz de línea de comandos para procesar tickets digitales de Mercadona.")
+public class Main implements Callable<Integer> {
+
+    @Option(names = {"--data-dir"}, description = "Directorio donde están los ficheros pdf con los tickets a analizar.", required = true, paramLabel = "<directorio-datos>")
+    private File dataDir;
+
+    @Override
+    public Integer call() throws Exception {
+        CommandLine.usage(this, System.out);
+        return 0;
+    }
+
+    @Command(name = "write-items-csv", 
+             mixinStandardHelpOptions = true,
+             description = "Genera un fichero csv con la información de todos los items comprados.")
+    public Integer writeItemsCsv(
+            @Option(names = {"--csv-file"}, description = "Nombre del fichero donde se escribe la información en csv.", required = true, paramLabel = "<fichero-csv>") File csvFile) {
+        // De momento solo el interfaz de linea de comandos, no enganchar con la lógica
+        System.out.println("Ejecutando operación write-items-csv...");
+        System.out.println("Directorio de datos: " + dataDir);
+        System.out.println("Fichero CSV: " + csvFile);
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
     }
 }
